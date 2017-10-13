d971fe76012ee9edbc2c54bcbb4047f9a578fa7a
Ethan Bjornsen <ethan.bjornsen@gmail.com>
2017-10-13 16:12:02 -0700

    Complete link-list exercise

--- hashmap/skeleton/lib/p04_linked_list.rb
+++ hashmap/skeleton/lib/p04_linked_list.rb
@@ -1,11 +1,11 @@
 class Node
-  attr_accessor :key, :val, :next, :prev
+  attr_accessor :key, :val, :prev, :next
 
   def initialize(key = nil, val = nil)
     @key = key
     @val = val
-    @next = nil
     @prev = nil
+    @next = nil
   end
 
   def to_s
@@ -13,13 +13,27 @@ class Node
   end
 
   def remove
-    # optional but useful, connects previous link to next link
+    # optional but useful, connects nextious link to prev link
     # and removes self from list.
+    return false if @prev == nil || @next == nil
+    @prev.next = @next
+    @next.prev = @prev
+    @val
   end
 end
 
+
+
+
+
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,30 +42,53 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next.equal?(@tail)  #simple identity comparison (pointer equality )
   end
 
   def get(key)
+    each { |link| return link.val if link.key == key }
   end
 
   def include?(key)
+    each { |link| return true if link.key == key }
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    old_last = last
+
+    @tail.prev = new_node
+    old_last.next = new_node
+    new_node.prev = old_last
+    new_node.next = @tail
+
   end
 
   def update(key, val)
+    each { |link| return link.val = val if link.key == key }
   end
 
   def remove(key)
+    each { |link| return link.remove if link.key == key }
+
   end
 
-  def each
+  def each(&prc)
+    current_node = @head.next
+    until current_node.equal?(@tail)
+      prc.call(current_node)
+      current_node = current_node.next
+    end
+    nil
   end
 
   # uncomment when you have `each` working and `Enumerable` included
